2746e518c783cf1255e6d0bfde95bb87a2468fcb
Daniel <josedaniel0908@gmail.com>
2026-05-25 02:46:44 -0400

    conexion con bd

--- config/conexion.php
+++ config/conexion.php
@@ -1,3 +1,9 @@
- <?php
-        echo "marico el que lo lea";
+<?php
+try{
+
+ $conexion = new PDO('mysql:host=localhost;dbname=plataforma_voluntariado', 'root', '');
+ echo "Conexión exitosa";
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
 ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'config/conexion.php';
+
+echo "El index.php funciona";
+
+?>
